7bc2d258ab086396a907006f869bd71aeccc6026
Jhansi M <janu5luckysiddhu@gmail.com>
2024-07-31 23:10:49 +0530

    commit

--- src/ATB7X_07072024/Ex_004.java
+++ src/ATB7X_07072024/Ex_004.java
@@ -3,7 +3,11 @@ package ATB7X_07072024;
 public class Ex_004 {
     public static void main(String[] args) {
 
+        System.out.println("Hello World");
+        System.out.println("Hello World");
 
+        System.out.print("Hello World");
+        System.out.print("Hello World");
     }
 
 
