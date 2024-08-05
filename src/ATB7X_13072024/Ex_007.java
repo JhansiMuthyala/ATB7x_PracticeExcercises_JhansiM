4c12ba0fafcaf08af5f31a71f08d543cad93584b
Jhansim <jhansi.muthyala440@gmail.com>
2024-08-05 21:10:03 +0530

    commit

---
+++ src/ATB7X_13072024/Ex_007.java
@@ -0,0 +1,9 @@
+package ATB7X_13072024;
+
+public class Ex_007 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/Assignements/Array_aasign1.java
@@ -0,0 +1,12 @@
+package Assignements;
+
+public class Array_aasign1 {
+
+    public static void main(String[] args) {
+        int i;
+        int j ;
+      //  String[i][j] pyramid;
+
+    }
+
+}
