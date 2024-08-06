2a182413b3c58c38cd04f6d2e6e0b4fa7f6e0411
Jhansim <jhansi.muthyala440@gmail.com>
2024-08-06 21:34:08 +0530

    commit

--- src/ATB7X_13072024/Ex_007.java
+++ src/ATB7X_13072024/Ex_007.java
@@ -7,10 +7,15 @@ public class Ex_007 {
         //max age of the person in world ? -
         byte age= 122; // age > 127 // less memory -> smart container - good practice
         int age1 = 100; // wasting memory - 214768348488
+        System.out.println("Byte age value: "+age);
+        System.out.println("int age1 value: "+age1);
 
         // program store pi with only 2 decimal 3.14, 3.142323237866999
         float pi = 3.14f;
+        System.out.println("float pi value: "+pi);
+
         double pi_d =  3.14876545678;
+        System.out.println("double pi_d value: "+pi_d);
 
     }
 }
