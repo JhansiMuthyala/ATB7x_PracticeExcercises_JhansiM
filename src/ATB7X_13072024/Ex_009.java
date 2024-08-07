4b728214107313177e1f25c7b320740961503d86
Jhansim <jhansi.muthyala440@gmail.com>
2024-08-07 15:27:46 +0530

    commit

---
+++ src/ATB7X_13072024/Ex_009.java
@@ -0,0 +1,22 @@
+package ATB7X_13072024;
+
+public class Ex_009 {
+    public static void main(String[] args) {
+        // max number stored in Byte
+        byte b = 127;
+        //b = b+1;
+        System.out.println(b);
+
+        //Data stored in Char , Alphabets ad special characters
+        char grade = 'A';
+        char grade_Sanam = 'E';
+        char grade_waseem = 'B';
+        char c = '!';
+        char c2 = '@';
+        char _123 = 'N';
+
+        // Can I change the data type of variable between program
+        // No
+        int age = 123;
+    }
+}
