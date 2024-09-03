9e3aa5585c73cc882766148090dbe0917d3cca51
Jhansim <jhansi.muthyala440@gmail.com>
2024-09-04 00:27:57 +0530

    commit

---
+++ src/ATB7X_13072024/Ex_012.java
@@ -0,0 +1,8 @@
+package ATB7X_13072024;
+
+public class Ex_012 {
+    public static void main(String[] args) {
+        String name = "Jhansi";
+        System.out.printf("Your name is -> %s",name);
+    }
+}
---
+++ src/ATB7X_13072024/Ex_017.java
@@ -0,0 +1,17 @@
+package ATB7X_13072024;
+
+public class Ex_017 {
+    public static void main(String[] args) {
+        // Escape Chars
+        char c = '\n'; // New line / Next Line
+        char c1 = '\t'; // Add Tab
+        char c2 = '\b'; // Backlash, delete 1 char
+        char c3 = '\r'; // Backlash, delete 1 word
+       System.out.println("Jhansi" + c3 + "Muthyala");
+        System.out.println("Jhansi" + c2 + "Muthyala");
+        System.out.println("Jhansi" + c1 + "Muthyala");
+        System.out.println("Jhansi" + c + "Muthyala");
+
+
+    }
+}
