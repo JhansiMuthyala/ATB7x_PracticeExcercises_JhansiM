8ed26c95c96c30a13d424ae7570ff5411f65ff1c
Jhansim <jhansi.muthyala440@gmail.com>
2024-09-16 23:48:52 +0530

    commit

---
+++ src/ATB7X_13072024/Ex_018.java
@@ -0,0 +1,16 @@
+package ATB7X_13072024;
+
+public class Ex_018 {
+    public static void main(String[] args) {
+
+//int enum = 112;
+   /// not possible as it is a keyword
+
+
+        int Enum = 112;
+        ///  possible as it is not a keyword
+
+    }
+
+
+}
