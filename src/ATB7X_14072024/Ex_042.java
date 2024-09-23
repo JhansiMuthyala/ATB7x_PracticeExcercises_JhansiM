ad2736b0bca57748508784aad3c47d778d4e73a7
Jhansim <jhansi.muthyala440@gmail.com>
2024-09-23 23:17:41 +0530

    commit

---
+++ src/ATB7X_14072024/Ex_042.java
@@ -0,0 +1,14 @@
+package ATB7X_14072024;
+
+public class Ex_042 {
+    public static void main(String[] args) {
+//Lab_059
+        int course = 100;
+        float GST = 18.45F;
+        float total_price = course+GST;// widening and we will have .45f data as well
+        System.out.println(total_price);
+
+
+    }
+
+}
